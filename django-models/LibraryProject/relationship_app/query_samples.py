c7f88fe8d0d7e2d9221793c1503d3ccf541c199f
Mohamedmv22 <vevomohamed0@gmail.com>
2026-02-01 00:47:38 +0200

    fix librarian_for_library query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -33,7 +33,7 @@ def librarian_for_library(library_name):
     """
     try:
         library = Library.objects.get(name=library_name)
-        librarian = library.librarian
+        librarian = Librarian.objects.get(library=library)
         return librarian
     except Library.DoesNotExist:
         return None
